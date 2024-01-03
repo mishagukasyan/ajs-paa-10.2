6b5199978abc8fe4cf3ee79b8335185ed89c8721
gukasyan <mishagukasyan@mail.ru>
2024-01-03 22:13:13 +0700

    change

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+import GameSavingLoader from './GameSavingLoader';
+
+(async () => {
+  try {
+    const saving = await GameSavingLoader.load();
+    const object = JSON.parse(saving);
+    Object.getPrototypeOf(object);
+  } catch (error) {
+    throw (new Error(error));
+  }
+})();
